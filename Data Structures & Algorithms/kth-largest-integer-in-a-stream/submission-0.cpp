d5f797fbaadd3695a3e8da9ad778b8333fbd1f94
Sumit Muley <sumitmuley95@gmail.com>
2026-07-06 23:07:55 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,18 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> minHeap;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int num : nums) add(num);
+    }
+    
+    int add(int val) {
+        if(minHeap.size() < k) minHeap.push(val);
+        else if(val > minHeap.top()) {
+            minHeap.pop();
+            minHeap.push(val);
+        }
+        return minHeap.top();
+    }
+};
