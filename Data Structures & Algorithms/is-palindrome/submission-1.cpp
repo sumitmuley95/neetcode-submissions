a0440ec51af1b5eed7faa51631e3954f79b0fb30
Sumit Muley <sumitmuley95@gmail.com>
2026-07-10 17:37:04 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for(int i = 0; i < s.length(); i++) {
+            if(isalpha(s[i]) || isdigit(s[i])) {
+                str += tolower(s[i]);
+            }
+        }
+        int i = 0;
+        int j = str.length()-1;
+        while(i <= j) {
+            if(str[i] != str[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
