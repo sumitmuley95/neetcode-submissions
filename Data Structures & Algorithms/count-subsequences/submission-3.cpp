a9f03a342ea7a2a1c04920bb85f51a0fdda28eee
Sumit Muley <sumitmuley95@gmail.com>
2026-07-26 16:39:45 +0530

    Add: count-subsequences - submission-3

---
+++ Data Structures & Algorithms/count-subsequences/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(string& s, string& t, int i, int j,vector<vector<int>>& dp) {
+        //base case
+        if(j == t.length()) return 1;
+        if(i == s.length()) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        //ek case main solve karuga baki recursion
+        int inc = 0;
+        if(s[i] == t[j]) {
+            inc = solve(s,t,i+1,j+1,dp);
+        }
+        int exc = solve(s,t,i+1,j,dp);
+        return dp[i][j] = inc+exc;
+    }
+    int numDistinct(string s, string t) {
+        vector<vector<int>>dp (s.length()+1,vector<int>(t.length()+1,-1));
+        return solve(s,t,0,0,dp);
+    }
+};
